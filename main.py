137f687150c7666328ccf0fd763fb04fa96db2ec
zmjm <zmjm@uchicago.edu>
2024-05-17 14:43:02 -0500

    refactor code to separate frame and painter modules

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
-from PIL import Image
 from utils import ColorFrame
 
 if __name__ == '__main__':
 
     root = Tk()
     root.withdraw()
-    file_path = askopenfilename(title='Select modified BMP Image template', filetypes=[('BMP Files', '*.bmp')])
+    file_path = askopenfilename(title='Select modified BMP Image template', filetypes=[('BMP Files', '*.bmp')],
+                                initialdir=os.getcwd())
     directory, filename = os.path.split(file_path)
     filename_new = os.path.splitext(filename)[0] + '_new.bmp'
 
@@ -18,4 +17,5 @@ if __name__ == '__main__':
     dmd_image.loadFromFile(file_path)
 
     # Show the converted DMD pattern and save it to your directory
-    dmd_image.saveDmdArrayToImage(directory, filename_new, save_template=False)
+    dmd_image.saveFrameToFile(directory, filename_new, save_template=False)
+    dmd_image.displayDmdSpaceImage()
